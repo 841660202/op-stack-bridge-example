b208cdc215ac0c5d33345f22d2a5f3618456113e
haotian.chen <haotian.chen@cmexpro.com>
2024-04-22 16:30:34 +0800

    feat: goerli -> sepolia

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,11 +1,11 @@
-import { createClient, configureChains, goerli } from "wagmi";
+import { createClient, configureChains, sepolia } from "wagmi";
 import { jsonRpcProvider } from "@wagmi/core/providers/jsonRpc";
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
 const RPC_URL = "https://rpc.eth.gateway.fm";
 
 const { provider, chains, webSocketProvider } = configureChains(
-  [goerli],
+  [sepolia],
   [
     jsonRpcProvider({
       rpc: (_) => ({
